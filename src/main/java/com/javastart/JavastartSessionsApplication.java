48079dac6bee7ce658a3a4ae731abb47c5c4b7e1
Krzysztof <krzubrycki@gmail.com>
2022-03-08 15:32:41 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartSessionsApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSessionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSessionsApplication.class, args);
+    }
+
+}
